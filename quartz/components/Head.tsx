85be645a237317530dfd60253675e10838111644
chartaseus <90235896+chartaseus@users.noreply.github.com>
2025-11-09 10:09:00 +0700

    Remove comment from Head

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -40,8 +40,6 @@ export default (() => {
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
-
-        {/*chartaseus gotta add these for now*/}
         <meta name="robots" content="noindex, nofollow"/>
         <meta name="referrer" content="same-origin"/>
 
