83beb94759b7e19ac09398f7391cc2bdcdff2537
chartaseus <90235896+chartaseus@users.noreply.github.com>
2025-11-16 17:20:55 +0700

    Fix build failure when a date prop is empty
    In my previously committed folder descriptions, the published dates are
    empty.

--- quartz/plugins/transformers/lastmod.ts
+++ quartz/plugins/transformers/lastmod.ts
@@ -18,7 +18,7 @@ const iso8601DateOnlyRegex = /^\d{4}-\d{2}-\d{2}$/
 function coerceDate(fp: string, d: any): Date {
   // `d` can be object because I use auto update modified date plugin and chose to record all modified dates in list form in the frontmatter, which then gets interpreted here as an array instead of string
   if (typeof d === "object") {
-    d = d.slice(0, 1)
+    d = d?.slice(0, 1)
   }
   // check ISO8601 date-only format
   // we treat this one as local midnight as the normal
